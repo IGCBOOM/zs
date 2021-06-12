da09cd0d17df85a02953fed073166fb1666bc90c
IGCBOOM <caya1.carter1@gmail.com>
2021-06-12 13:11:03 -0500

    Initial commit

---
+++ code/ZSGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+
+namespace zs
+{
+	[Library( "zs", Title = "Zombie Survival" )]
+	partial class ZSGame : Game
+    {
+
+	    public ZSGame()
+	    {
+
+			
+
+	    }
+
+	    public override void DoPlayerNoclip( Client player )
+	    {
+		    return;
+	    }
+
+    }
+}
